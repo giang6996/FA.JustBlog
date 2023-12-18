fa2d33cf3955c99cd7ee9efd0e2b4ecabce803f1
anhducnguyen091103 <153896435+anhducnguyen091103@users.noreply.github.com>
2023-12-18 09:42:09 +0700

    Add files via upload

---
+++ FA.JustBlog/Controllers/CategoryController.cs
@@ -0,0 +1,134 @@
+﻿using FA.JustBlog.Core;
+using Microsoft.AspNetCore.Mvc;
+using FA.JustBlog.Models;
+
+
+namespace FA.JustBlog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public IActionResult Index()
+        {
+            var categorys = _categoryService.GetAll();
+            var categoryVms = new List<CategoryViewModel>();
+            foreach (var category in categorys)
+            {
+                categoryVms.Add(new CategoryViewModel()
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    UrlSlug = category.UrlSlug,
+                    Description = category.Description,
+                });
+            }
+            return View(categoryVms);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name, string urlSlug, string description)
+        {
+            _categoryService.Add(new Category()
+            {
+                Name = name,
+                UrlSlug = urlSlug,
+                Description = description
+            });
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detail(int id)
+        {
+            try
+            {
+                var category = _categoryService.Find(id);
+                if (category != null)
+                {
+                    var categoryVms = new CategoryViewModel()
+                    {
+                        Id = category.Id,
+                        Name = category.Name,
+                        UrlSlug = category.UrlSlug,
+                        Description = category.Description,
+                    };
+                    return View(categoryVms);
+                }
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var category = _categoryService.Find(id);
+                if (category != null)
+                {
+                    var categoryModel = new CategoryViewModel() { 
+                        Name = category.Name, 
+                        UrlSlug = category.UrlSlug, 
+                        Description = category.Description };
+                    return View(categoryModel);
+                }
+            }
+            catch
+            {
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, CategoryViewModel categoryViewModel)
+        {
+            try
+            {
+                var category = new Category()
+                {
+                    Id = id,
+                    Name = categoryViewModel.Name,
+                    Description = categoryViewModel.Description,
+                    UrlSlug = categoryViewModel.UrlSlug,
+                };
+
+                _categoryService.Update(category);
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var category = _categoryService.Find(id);
+                if (category != null)
+                    _categoryService.Delete(category);
+            }
+            catch
+            {
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FA.JustBlog/Controllers/TagController.cs
@@ -0,0 +1,140 @@
+﻿using FA.JustBlog.Core;
+using FA.JustBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FA.JustBlog.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ITagService _tagService;
+        public TagController(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+        public IActionResult Index()
+        {
+            var tags = _tagService.GetAll();
+            var tagVms = new List<TagViewModel>();
+            foreach (var tag in tags)
+            {
+                tagVms.Add(new TagViewModel()
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    UrlSlug = tag.UrlSlug,
+                    Description = tag.Description,
+                    Count = tag.Count,
+                });
+            }
+            return View(tagVms);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name, string urlSlug, string description, int count)
+        {
+            _tagService.Add(new Tag()
+            {
+                Name = name,
+                UrlSlug = urlSlug,
+                Description = description,
+                Count = count
+            });
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detail(int id)
+        {
+            try
+            {
+                var tag = _tagService.Find(id);
+                if (tag != null)
+                {
+                    var tagVms = new TagViewModel()
+                    {
+                        Id = tag.Id,
+                        Name = tag.Name,
+                        UrlSlug = tag.UrlSlug,
+                        Description = tag.Description,
+                        Count= tag.Count
+                    };
+                    return View(tagVms);
+                }
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var tag = _tagService.Find(id);
+                if (tag != null)
+                {
+                    var tagModel = new TagViewModel()
+                    {
+                        Name = tag.Name,
+                        UrlSlug = tag.UrlSlug,
+                        Description = tag.Description,
+                        Count = tag.Count
+                    };
+                    return View(tagModel);
+                }
+            }
+            catch
+            {
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, TagViewModel tagViewModel)
+        {
+            try
+            {
+                var tag = new Tag()
+                {
+                    Id = id,
+                    Name = tagViewModel.Name,
+                    Description = tagViewModel.Description,
+                    UrlSlug = tagViewModel.UrlSlug,
+                    Count = tagViewModel.Count
+                };
+
+                _tagService.Update(tag);
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var tag = _tagService.Find(id);
+                if (tag != null)
+                    _tagService.Delete(tag);
+            }
+            catch
+            {
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
